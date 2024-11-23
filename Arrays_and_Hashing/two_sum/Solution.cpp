459f331b97c19738c42531e9782213e2e79be8c7
= <ayoubelmahfoudi05@gmail.com>
2024-11-23 17:23:52 +0100

    Added solution for two_sum problem.

--- Arrays_and_Hashing/two_sum/Solution.cpp
+++ Arrays_and_Hashing/two_sum/Solution.cpp
@@ -1,8 +1,22 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
-  public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-      return std::vector<int>(0,0);
+public:
+  std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::vector<int> result;
+        int difference;
+        std::unordered_map<int, int> mapping;
+        for (int i = 0; i < nums.size(); i++) {
+            difference = target - nums[i];
+            if (mapping.find(difference) != mapping.end()) {
+                result.push_back(mapping[difference]);
+                result.push_back(i);
+                break;
+            } else {
+                mapping[nums[i]] = i;
+            }
+        }
+        return result;
     }
 };
