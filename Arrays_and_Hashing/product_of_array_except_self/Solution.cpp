9297d77d93078b978a7ec20ac049411d12ddf7b9
= <ayoubelmahfoudi05@gmail.com>
2024-12-03 21:25:53 +0100

    Added solution for product_of_array_except_self problem.

--- Arrays_and_Hashing/product_of_array_except_self/Solution.cpp
+++ Arrays_and_Hashing/product_of_array_except_self/Solution.cpp
@@ -1,9 +1,26 @@
-#include <vector>
-
 class Solution {
 public:
-    std::vector<int> productExceptSelf(std::vector<int>& nums) {
-      std::vector<int> result;
-      return result;
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int product = 1, zeroCount = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                product *= num;
+            } else {
+                zeroCount++;
+            }
+        }
+        if (zeroCount > 1) {
+            return std::vector<int> (nums.size(),0);
+        }
+        std::vector<int> result(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            if (zeroCount > 0) {
+                result[i] = (nums[i] == 0) ? product : 0;
+            } else {
+                result[i] = product / nums[i];
+            }
+        }
+        return result;
     }
 };
+
