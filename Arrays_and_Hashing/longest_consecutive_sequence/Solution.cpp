187b8d841836123362d205046ef969400fff3456
= <ayoubelmahfoudi05@gmail.com>
2024-12-05 20:29:36 +0100

    Added solution for longest_consecutive_sequence.

--- Arrays_and_Hashing/longest_consecutive_sequence/Solution.cpp
+++ Arrays_and_Hashing/longest_consecutive_sequence/Solution.cpp
@@ -1,9 +1,22 @@
+#include <list>
 #include <vector>
+#include <unordered_set>
 
 class Solution {
   public:
     int longestConsecutive(std::vector<int>& nums) {
-      int result;
-      return result;
+      int max = 0;
+      std::list<int> Sequence;
+      std::unordered_set<int> values(nums.begin(),nums.end(), nums.size());
+      for (int i = 0; i < nums.size(); i++) {
+        int val = nums[i];  
+        while (values.count(val) > 0) {
+          Sequence.push_back(val);
+          val++;
+        }
+        max = (max >= Sequence.size()) ? max : Sequence.size();
+        Sequence.clear();
+      } 
+      return max;
     }
 };
