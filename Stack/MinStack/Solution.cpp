9dacab99b888059384e6cf2a579ed0671f3d0310
= <ayoubelmahfoudi05@gmail.com>
2024-12-08 19:59:41 +0100

    Added solution for the MinStack implementation problem.

--- Stack/MinStack/Solution.cpp
+++ Stack/MinStack/Solution.cpp
@@ -1,18 +1,33 @@
+#include <stack>
+
 class MinStack {
+  std::stack<int> mainStack;
+  std::stack<int> extraStack;
+
   public:
     MinStack() {
-
+  
     }
     
     void push(int val) {
-
+      mainStack.push(val);
+      if (extraStack.empty()) {
+        extraStack.push(val);
+      } else {
+        int min = (extraStack.top() < val ? extraStack.top() : val);
+        extraStack.push(min);
+      }
     }
     
     void pop() {
-
+      mainStack.pop();
+      extraStack.pop();
     }
     
     int top() {
-
+      return mainStack.top();
+    }
+    int getMin() {
+      return extraStack.top();
     }
 };
