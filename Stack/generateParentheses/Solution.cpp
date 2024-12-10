7fb0550d42a1a24f78d87cd876723ecbf67be372
= <ayoubelmahfoudi05@gmail.com>
2024-12-10 19:37:09 +0100

    Start working on the generateParentheses problem.

---
+++ Stack/generateParentheses/Solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+  std::vector<std::string> generateParenthesis(int n) {
+    std::vector<std::string> result;
+    return result;
+  }
+};
